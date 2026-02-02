bcc547faf4a05a1f0dca2f7aac9f3081921f259a
WyattAu <wyatt_au@protonmail.com>
2026-02-02 02:57:45 +0000

    Added Keychain and SSH config logic

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,5 +1,19 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
+if status is-interactive
+    # ... existing starship init ...
 
+    # 1. Set up graphical password prompt (KDE Wallet)
+    set -x SSH_ASKPASS /usr/bin/ksshaskpass
+    set -x SSH_ASKPASS_REQUIRE force  # Forces the GUI prompt
+
+    # 2. Initialize Keychain
+    # --quiet: Don't spam terminal on open
+    # --agents ssh: We only care about SSH, not GPG
+    # List your keys here (id_forgejo, id_ed25519)
+    if type -q keychain
+        keychain --eval --quiet --agents ssh id_forgejo id_ed25519 | source
+    end
+enD
 if type -q direnv
     direnv hook fish | source
 end
@@ -8,3 +22,8 @@ end
 #function fish_greeting
 #    # smth smth
 #end
+# "Nuke" a package: Removes package + unused dependencies + config files
+alias nuke="sudo pacman -Rns"
+
+# Clean up "Orphans" (Dependencies left behind by other uninstalls)
+alias cleanup="sudo pacman -Qtdq | sudo pacman -Rns -"
